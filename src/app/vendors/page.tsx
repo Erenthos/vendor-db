7da7bb69d6d4e6f70c583a5473760587f5a7fd08
Erenthos <erenthos@gmail.com>
2025-12-03 16:55:49 +0530

    Update page.tsx

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -1,1 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function VendorsPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, [router]);
+
+  return null;
+}
 
