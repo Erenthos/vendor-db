027c93c22f6450fe0ad944082fa5138ec9ba1a22
Erenthos <erenthos@gmail.com>
2025-12-03 16:47:44 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', 'ui-sans-serif', 'Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
 
